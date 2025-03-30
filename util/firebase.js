17cdc946fe5477d9c513a72b4236ddc7261beb07
Muhammad khattab <muhammadkh.15.9.2000@gmail.com>
2025-03-30 23:42:27 +0300

    change serviceAccount from requiring from json file to env variable

--- util/firebase.js
+++ util/firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./fahim-5a1d6-firebase-adminsdk-fbsvc-006b735a39.json'); // Replace with actual path
+// const serviceAccount = require('./fahim-5a1d6-firebase-adminsdk-fbsvc-006b735a39.json'); // Replace with actual path
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
